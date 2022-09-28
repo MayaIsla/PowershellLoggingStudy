ed872caf93f952287a2726d9b5fa79f1ac289345
Maya Islambouli <43270477+MayaIsla@users.noreply.github.com>
2022-09-28 15:34:33 -0500

    Add files via upload

---
+++ loggingStudy.ps1
@@ -0,0 +1,5 @@
+﻿$TS = "[{0:MM/dd/yy} {0:HH:mm:ss}]" -f (Get-Date)
+$logFile = 'C:\Users\mislambo\OneDrive - Western Alliance Bank\Desktop\hi\logs.log'
+Write-Output "$TS I am running a script. " >> $logFile
+Start-Sleep -s 30
+Write-Output "$TS Paused for 30 seconds. " >> $logFile
