a237f2c05f58186eb28d318288e228c823afd7b0
Maya Islambouli <43270477+MayaIsla@users.noreply.github.com>
2022-09-28 15:34:59 -0500

    Update loggingStudy.ps1

--- loggingStudy.ps1
+++ loggingStudy.ps1
@@ -1,5 +1,5 @@
 ﻿$TS = "[{0:MM/dd/yy} {0:HH:mm:ss}]" -f (Get-Date)
-$logFile = 'C:\Users\mislambo\OneDrive - Western Alliance Bank\Desktop\hi\logs.log'
+$logFile = 'C:\path\to\log\log.log'
 Write-Output "$TS I am running a script. " >> $logFile
 Start-Sleep -s 30
 Write-Output "$TS Paused for 30 seconds. " >> $logFile
